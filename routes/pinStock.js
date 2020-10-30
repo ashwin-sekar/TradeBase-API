35198705454f18d7e70ffbe734ce90bea177df52
ashwin-sekar <ashwinsekar14@gmail.com>
2020-10-30 09:20:41 +0530

    fixes

--- routes/pinStock.js
+++ routes/pinStock.js
@@ -3,13 +3,10 @@ const PinStock = require('../models/pinstock.model');
 const pinStock = async(req,res) => {
     const { symbol, user } = req.body;
 
+    console.log(req.body);
+
     if(!symbol || !user ) {
         return res.status(401).json({msg: "Invalid request"});
-    }
-
-    const exist = PinStock.find({symbol: symbol, user: user});
-    if(exist) {
-        return res.status(401).json({msg: "You have already pinned this stock"});
     }
 
     const newStock = new PinStock({
